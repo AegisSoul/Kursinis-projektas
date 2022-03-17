4d529514008cd5f9464fefed09844c3053aef9e6
Erne <ernestastukas@gmail.com>
2022-03-17 02:01:31 +0200

    created team routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/teams', [TeamController::class, 'index']);
+Route::get('/teams/add', [TeamController::class, 'viewAddForm']);
+Route::post('/teams/add', [TeamController::class, 'store']);
+Route::get('/teams/edit/{id}', [TeamController::class, 'viewEditForm']);
+Route::post('/teams/edit/{id}', [TeamController::class, 'edit']);
+Route::get('/teams/delete/ask/{id}', [TeamController::class, 'deleteForm']);
+Route::get('/teams/delete/{id}', [TeamController::class, 'delete']);
+Route::get('/teams/search/{query}', [TeamController::class, 'search']);
+
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+// TODO: button animation
+// TODO: form - field - validation - error
+// TODO: extra styling and stuff
