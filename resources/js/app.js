f789820d75eab852172d399e2d566784740e42ea
Erne <ernestastukas@gmail.com>
2022-03-27 18:04:44 +0300

    Added a functioning "back to top" button.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const backToTop = document.querySelector('#backToTop');
+
+
+backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+
+
+})
--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -10,6 +10,7 @@
                 <a href="/teams/add" class="ml-2 btn px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-blue-600 hover:text-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-blue-300 dark:hover:text-gray-600" type="button">Add a player</a>
 
             </div>
+            <a id="backToTop" class="fixed right-40 bottom-6 ml-2 btn px-6 py-2 border-2 bg-white opacity-75 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-blue-600 hover:text-white hover:opacity-100 focus:outline-none focus:ring-0 transition duration-150 ease-in-out dark:bg-gray-700  dark:text-gray-200 dark:hover:bg-blue-300 dark:hover:text-gray-600 hover:cursor-pointer" type="button">Back to top</a>
         </div>
     </div>
 </form>
@@ -45,6 +46,8 @@
         <tr class="text-gray-500 whitespace-nowrap hover:bg-green-500 hover:bg-opacity-20 dark:text-white dark:hover:text-green-400 dark:hover:bg-gray-800">
         @elseif($team -> Team == "Lietkabelis")
         <tr class="hover:bg-rose-900 hover:bg-opacity-20 dark:hover:bg-gray-800 text-gray-500 whitespace-nowrap dark:text-white dark:hover:text-rose-600 dark:hover:text-opacity-90">
+        @elseif($team -> Team == "Neptūnas")
+        <tr class="hover:bg-blue-600 hover:bg-opacity-30 dark:hover:bg-gray-800 text-gray-500 whitespace-nowrap dark:text-white dark:hover:text-blue-500 dark:hover:text-opacity-90">
         @else
         <tr class="hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-500 whitespace-nowrap dark:text-white ">
         @endif
@@ -53,7 +56,7 @@
             <td class="py-4 px-6 text-sm font-medium">{{$team -> Team}}</td>         
             <td class="py-4 px-6 text-sm font-medium">{{$team -> Position}}</td>
             @if($team -> Height > 5)
-            <td class="py-4 px-6 text-sm font-mediu">{{$team -> Height}}cm</td>
+            <td class="py-4 px-6 text-sm font-medium">{{$team -> Height}}cm</td>
             @else
             <td class="py-4 px-6 text-sm font-medium">{{$team -> Height}}m</td>
             @endif
