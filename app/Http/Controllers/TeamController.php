f3af721f2dd6da963e1a65d5232bc1410affa2bf
Erne <ernestastukas@gmail.com>
2022-06-13 11:02:53 +0300

    Administratorius

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,28 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\IsAdmin;
 use App\Models\Team;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
-{   
+{
 
     public function __construct()
     {
-        $this -> middleware(['auth'])->except(['index']);
+        $this->middleware(['auth'])->except(['index']);
+        $this->middleware([IsAdmin::class])->only(['viewAddForm', 'store', 'edit', 'viewEditForm', 'deleteForm', 'delete']);
     }
-    
-    public function index(){
+
+    public function index()
+    {
         $teams = Team::all();
-        return view ('teams', compact('teams'));
-        
+        return view('teams', compact('teams'));
     }
 
-    public function viewAddForm(){
-        return view ('add_teams');
+    public function viewAddForm()
+    {
+        return view('add_teams');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // Validation
         $validated = $request->validate([
             'First-name' => 'required|max:100',
@@ -41,13 +45,13 @@ class TeamController extends Controller
             'Position' => request('Position'),
             'Height' => request('Height'),
             'Average' => request('Average')
-        ]); 
+        ]);
 
         return redirect('/teams');
     }
 
     public function viewEditForm($id)
-    {   
+    {
         $team = Team::where('id', $id)->firstOrFail();
         return view('edit_teams', compact('team'));
     }
@@ -65,32 +69,35 @@ class TeamController extends Controller
 
         $team = Team::where('id', $id)->firstOrFail();
 
-        $team -> FirstName = request('First-name');
-        $team -> LastName = request('Last-name');
-        $team -> Team = request('Team');
-        $team -> Position = request('Position');
-        $team -> Height = request("Height");
-        $team -> Average = request("Average");
-        $team -> save();
-        
+        $team->FirstName = request('First-name');
+        $team->LastName = request('Last-name');
+        $team->Team = request('Team');
+        $team->Position = request('Position');
+        $team->Height = request("Height");
+        $team->Average = request("Average");
+        $team->save();
+
 
         return redirect('/teams');
     }
 
-    public function deleteForm($id){
+    public function deleteForm($id)
+    {
         $team = Team::where('id', $id)->firstOrFail();
         return view('delete_teams', compact('team'));
     }
-    public function delete($id){
+    public function delete($id)
+    {
         $team = Team::where('id', $id)->firstOrFail();
-        $team -> delete();
-        
+        $team->delete();
+
         return redirect('/teams');
     }
-    public function search(){
-        $teams = Team::where('FirstName', 'LIKE', '%'.$_GET['query'].'%')
-        ->orWhere('Team', $_GET['query'])->get();
+    public function search()
+    {
+        $teams = Team::where('FirstName', 'LIKE', '%' . $_GET['query'] . '%')
+            ->orWhere('Team', $_GET['query'])->get();
 
-        return view ('teams', compact('teams'));
+        return view('teams', compact('teams'));
     }
 }
